715bd8b9ebb5709c97e6aa5f82d8084714f1a9c4
hiyongrui <172808H@mymail.nyp.edu.sg>
2018-11-15 00:56:26 +0800

    intent filter to view another activity

---
+++ app/src/main/java/com/example/tanyongrui/basicmovie/viewMovieActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tanyongrui.basicmovie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class viewMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_movie)
+    }
+}
