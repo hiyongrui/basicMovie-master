509d678f32bc5161b6b7cf87cc85b7fe676ea11f
hiyongrui <172808H@mymail.nyp.edu.sg>
2018-11-16 00:03:42 +0800

    done last screen, somehow have audio error then crash, verify in sch com tmr

---
+++ app/src/main/java/com/example/tanyongrui/basicmovie/rateMovieActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tanyongrui.basicmovie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class rateMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rate_movie)
+    }
+}
